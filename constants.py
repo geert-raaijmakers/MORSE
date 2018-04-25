f227b228f88f7cad8200206f377cf5954ad6b0fa
Geert Raaijmakers <raaymakers8@hotmail.com>
2018-04-25 16:47:33 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,17 @@
+### Constants used throughout my project code ###
+
+c = 3e10
+G = 6.67428e-8
+Msun = 1.989e33
+rho_0 = 10**14.3
+rho_ns = 2.7e14
+rho_1 = 1.85 * rho_ns 
+rho_2 = 2. * rho_1 
+#rho_2 = 1.8 * rho_1 
+rho_3 = 2. * rho_2 
+P_0 = 1.5281267425e+33
+gamma_0 = 2.68019358431
+dyncm2_to_MeVfm3 = 1./(1.6022e33)
+gcm3_to_MeVfm3 = 1./(1.7827e12)
+oneoverfm_MeV = 197.33
+
